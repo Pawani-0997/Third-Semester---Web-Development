7bc29534c67195fd276dc69d1a7c65439e584423
Pawani Dubey <pawanidubey06@gmail.com>
2026-09-03 10:59:11 +0530

    Updated Promise.js
    Updated Promise.js

--- promise.js
+++ promise.js
@@ -1,11 +1,19 @@
-let promise = new Promise((resolve,reject)=>{
+/*let promise = new Promise((resolve,reject)=>{
     console.log("I am a new promise");
 });
 resolve("Successfully executed");
 resolve("123");
 reject("Reject your request");
+*/
+function GetData(dataId) {
+    return new Promise((resolve, reject) => {
 
-function GetData(dataId)
-{
-    return new Promise((resolve,reject)=>{})
+        setTimeout(() => {
+            console.log("data", dataId);
+            resolve("successful");
+        }, 8000);
+
+    });
 }
+
+let r = GetData(123);
