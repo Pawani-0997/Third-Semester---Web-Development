e1399de918b0ab3718a790462ebcc449e63abd4a
Pawani Dubey <pawanidubey06@gmail.com>
2026-09-03 11:20:54 +0530

    Updated new_promise.js
    Updated new_promise.js

--- new_promise.js
+++ new_promise.js
@@ -2,17 +2,14 @@ const GetPromise=()=>{
     return new Promise((resolve,reject)=>{
     console.log("I am a new Promise");
     resolve("successful")
-    //reject("network error");
+   // reject("network error");
     });
 };
 
-GetPromise();
-/*
 let promise=GetPromise();
 promise.then(()=>{
     console.log("Promise is fullfilled");
 });
 promise.catch(()=>{
-    console.log();
+    console.log("Network is not working properly");
 });
-*/
