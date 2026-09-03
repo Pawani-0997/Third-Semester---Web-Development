b0ebef2be1aa6825198b73d97ee79fd10434cb4e
Pawani Dubey <pawanidubey06@gmail.com>
2026-09-03 11:01:46 +0530

    Addition.js
    Addition.js

---
+++ addition.js
@@ -0,0 +1,106 @@
+// ADD TWO NUMBERS AND ADD TWO STRINGS
+/*
+let a = 5
+let b = 5
+
+let add = a+b
+
+console.log(add);
+
+a = "Pawani"
+
+b = " Dubey"
+
+c = a + b
+
+console.log(c);
+
+a = 10
+
+b = "abc"
+
+c = a + b
+
+console.log(c);
+
+const Fruits = ['Apple','Banana','Orange','Pineapple','Guava','Pomegranate','Kiwi']
+
+for(let i =0 ; i < 7 ; i++)
+{
+    console.log(Fruits[i]);
+}
+
+Fruits.push('Papaya');
+
+console.log(Fruits[7]);
+
+console.log(Fruits.indexOf('WaterMelon'));
+
+const a = [1,2,3,4,5,6,7]
+const b = a.slice(2,5)
+const c = a.splice(2,5);
+console.log(b);
+console.log(c);
+
+console.log(a);
+
+const a = [1,2,3]
+
+const b = ['a','b','c']
+
+const Fruits = ['Apple','Banana','Orange','Pineapple','Guava','Pomegranate','Kiwi']
+
+//const c = a.concat(b)
+
+//console.log(c);
+
+const r = [...a,...b,...Fruits]
+
+console.log(r);
+
+const Student = {
+    name: "abc" ,
+    email: "abc@gmail.com" , 
+    rollno: 1234 , 
+    batch: "CSE - 26"
+}
+
+console.log(Student);
+console.log(Student.rollno);
+
+function sum(a,b)
+{
+    let sum;
+    sum = a + b;
+    return sum;
+}
+
+console.log(sum(5,5));
+
+function greet(fname , lname)
+{
+   console.log("Good Morning "+fname+lname);
+}
+
+//let f = "Pawani"
+//let l = "Dubey"
+
+function greet(fname , lname)
+{
+  //let r = fname + lname;
+  console.log(`Good Morning ${fname} ${lname}`);
+}
+greet("Pawani","Dubey");
+
+alert('Welcome');
+
+const arr= [1,10,5,8,6];
+
+for(let i =0;i<arr.length;i++)
+{
+    console.log(arr[i]);
+}
+
+*/
+
+const a = [10,20,30,40];
