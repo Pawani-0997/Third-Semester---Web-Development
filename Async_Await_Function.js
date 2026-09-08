0785621f44c9a7b32599a6210fe4aa22feef85b4
Pawani Dubey <pawanidubey06@gmail.com>
2026-09-08 16:16:28 +0530

    Updated async function
    Updated async function

--- Async_Await_Function.js
+++ Async_Await_Function.js
@@ -15,3 +15,24 @@ return GetData(2);})
 return GetData(3);})
 .then((res)=>{
 console.log(res);})
+
+async function hello()
+{
+    console.log("hello!");
+}
+
+function Api()
+{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("weatherData, i found");
+            resolve(200);
+        },3000);
+    },);
+}
+
+async function getweatherData() {
+    await Api();
+    await Api();
+}
+getweatherData();
