9a4312c24c5d0f202382f37db7a7e92308f2439c
Pawani Dubey <pawanidubey06@gmail.com>
2026-09-09 10:12:14 +0530

    Updated file writing
    Updated file writing

--- write_file.js
+++ write_file.js
@@ -3,8 +3,15 @@ const f=require("fs");
 console.log("data1");
 console.log("data2");
 
-f.writeFileSync("pawani.txt","hello to all, i am pawani dubey");
+//f.writeFileSync("pawani.txt","hello to all, i am pawani dubey");
 //f.appendFileSync("pawani2.txt","\nupdated data");
 //f.unlinkSync("pawani.txt");
 console.log("data3");
 console.log("data4");
+
+f.promises.writeFile("File_out.txt","new data","utf-8")
+.then(console.log("file successfully created"))
+.catch((err)=>console.log(err))
+//read a file
+//f.promise.readFile("File_out.txt","utf-8")
+//.then((data)=>console.log(data))
