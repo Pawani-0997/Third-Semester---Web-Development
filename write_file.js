026ebc1240606a2cdd30b6452877deb824f98f24
Pawani Dubey <pawanidubey06@gmail.com>
2026-09-09 10:06:35 +0530

    file writing
    file writing

---
+++ write_file.js
@@ -0,0 +1,10 @@
+//npm - node package manager
+const f=require("fs");
+console.log("data1");
+console.log("data2");
+
+f.writeFileSync("pawani.txt","hello to all, i am pawani dubey");
+//f.appendFileSync("pawani2.txt","\nupdated data");
+//f.unlinkSync("pawani.txt");
+console.log("data3");
+console.log("data4");
