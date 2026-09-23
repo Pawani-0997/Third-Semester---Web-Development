366a7b4b5fdec14ec90a10cec1835d987c5072f9
Pawani Dubey <pawanidubey06@gmail.com>
2026-09-23 10:04:00 +0530

    Updated Experiment 5 using post operation
    Updated Experiment 5 using post operation

--- PostOperation.js
+++ PostOperation.js
@@ -1,21 +1,27 @@
 const express = require('express');
+
 const app = express();
 
 app.use(express.json());
 
 let students = [
-    {id:1 , name:"Rahul" , branch:"CSE"},
-    {id:2 , name:"Aman", branch:"IT"}
+    {id: 1, name: "Rahul", branch: "CSE"},
+    {id: 2, name: "Aman", branch: "IT"}
 ];
 
-app.get('/',(req,res) => {
-    res.setDefaultEncoding("Server is running");
+app.get('/', (req, res) => {
+    res.send("Server is running");
 });
 
-app.post('/students',(req,res) => {
+app.post('/students', (req, res) => {
     const newStudent = req.body;
+
     students.push(newStudent);
-    res.status(201).json({message: "Student added successfully", student: newStudent });
+
+    res.status(201).json({
+        message: "Student added successfully",
+        student: newStudent
+    });
 });
 
 app.listen(3005, () => {
